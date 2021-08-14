50e1d8b4ea32bef71ebbe05b8274a0b0ae750810
KVADRAT <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-08-14 19:52:08 +0300

    level-2/14.08.2021/1

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -3,7 +3,19 @@ import './AboutMe.css';
 function AboutMe() {
   return (
     <div className="AboutMe">
-        AboutMe
+        <h2 className='AboutMe__title'>Студент</h2>
+
+        <article className='AboutMe__paragraph'>subtitle
+          <h2 className='AboutMe__paragraph-title'>Владимир</h2>
+          <h3 className='AboutMe__paragraph-subtitle'>Фронтенд-разработчик, 23 года</h3>
+          <p className='AboutMe__description'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
+и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+        </article>
+        <div className='AboutMe__connection'>
+          <a href={'https://developer.mozilla.org/en-US/docs/Web/CSS/font-style'}>Facebook</a>
+          <a href={'https://hcdev.ru/grid/grid-5/'}>Github</a>
+        </div>
+        <img></img>
     </div>
   );
 }
--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -3,7 +3,20 @@ import './Techs.css';
 function Techs() {
   return (
     <div className="Techs">
-        Techs
+        <h2 className='Techs__title'>Технологии</h2>
+        <article className='Techs__paragraph'>
+          <h3 className='Tecs__paragraphh-title'>7 технологий</h3>
+          <p className='Techs__description'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+        </article>
+        <ul className='Techs__list'>
+          <li className='Techs__item'>HTML</li>
+          <li className='Techs__item'>CSS</li>
+          <li className='Techs__item'>JS</li>
+          <li className='Techs__item'>React</li>
+          <li className='Techs__item'>Git</li>
+          <li className='Techs__item'>Express.js</li>
+          <li className='Techs__item'>mongoDB</li>
+        </ul>
     </div>
   );
 }
