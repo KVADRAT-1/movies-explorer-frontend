d7a711f7f5372dc028ff00b117b860b8b515009e
KVADRAT <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-08-16 14:29:35 +0300

    level-2/16.08.2021/1

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -4,18 +4,39 @@ function AboutMe() {
   return (
     <div className="AboutMe">
         <h2 className='AboutMe__title'>Студент</h2>
-
-        <article className='AboutMe__paragraph'>subtitle
-          <h2 className='AboutMe__paragraph-title'>Владимир</h2>
-          <h3 className='AboutMe__paragraph-subtitle'>Фронтенд-разработчик, 23 года</h3>
-          <p className='AboutMe__description'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
-и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-        </article>
-        <div className='AboutMe__connection'>
-          <a href={'https://developer.mozilla.org/en-US/docs/Web/CSS/font-style'}>Facebook</a>
-          <a href={'https://hcdev.ru/grid/grid-5/'}>Github</a>
+        <div className='AboutMe__student'>
+          <div className='AboutMe__photo' alt='photo'/>
+          <article className='AboutMe__paragraph'>
+            <h2 className='AboutMe__paragraph-title'>Владимир</h2>
+            <h3 className='AboutMe__paragraph-subtitle'>Фронтенд-разработчик, 23 года</h3>
+            <p className='AboutMe__description'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <div className='AboutMe__website'>
+            <a className='AboutMe__link' href={'https://developer.mozilla.org/en-US/docs/Web/CSS/font-style'}>Facebook</a>
+            <a className='AboutMe__link' href={'https://developer.mozilla.org/en-US/docs/Web/CSS/font-style'}>Github</a>
+          </div>
+          </article>
         </div>
-        <img></img>
+        <div className='AboutMe__portfolio'>Портфолио</div>
+        <ul className='AboutMe__portfolio-list'>
+          <li className='AboutMe__portfolio-item'>
+            <a className='AboutMe__portfolio-link' href={'https://developer.mozilla.org/en-US/docs/Web/CSS/font-style'}>
+              <p className='AboutMe__portfolio-text'>Статичный сайт</p>
+              <div className='AboutMe__portfolio-arrow'/>
+            </a>
+          </li>
+          <li className='AboutMe__portfolio-item'>
+            <a className='AboutMe__portfolio-link' href={'https://developer.mozilla.org/en-US/docs/Web/CSS/font-style'}>
+              <p className='AboutMe__portfolio-text'>Адаптивный сайт</p>
+              <div className='AboutMe__portfolio-arrow'/>
+            </a>
+          </li>
+          <li className='AboutMe__portfolio-item'>
+            <a className='AboutMe__portfolio-link' href={'https://developer.mozilla.org/en-US/docs/Web/CSS/font-style'}>
+              <p className='AboutMe__portfolio-text'>Одностраничное приложение</p>
+              <div className='AboutMe__portfolio-arrow'/>
+            </a>
+          </li>
+        </ul>
     </div>
   );
 }
