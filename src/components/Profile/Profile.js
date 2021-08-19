85acedaf68ef4b619943385eb83f2a9ced7172a8
KVADRAT <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-08-19 20:27:16 +0300

    level-2/19.08.2021/2

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,48 @@
 import './Profile.css';
 
 function Profile() {
+
+  function switchEdit(e) {
+    const profile = document.querySelector('.Profile')
+    const buttonForm = profile.querySelector('.Profile__button-form')
+    const profileButtons = profile.querySelector('.Profile__buttons')
+    const inputAll = profile.querySelectorAll('.Profile__input')
+
+    if (e.target.className === 'Profile__button-edit') {
+      buttonForm.style.display = 'block'
+      profileButtons.style.display = 'none'
+      inputAll.forEach((item) => {
+        item.removeAttribute('readOnly')
+        item.style.cursor = 'pointer';
+      })
+    } else {
+      buttonForm.style.display = 'none'
+      profileButtons.style.display = 'flex'
+      inputAll.forEach((item) => {
+        item.setAttribute('readOnly', 'readonly')
+        item.style.cursor = 'default';
+      })
+    }
+  }
+
   return (
-    <div className="Profile">
-      → → → Profile ← ← ←
+    <div className='Profile'>
+      <h2 className='Profile__title'>Привет, Виталий!</h2>
+      <form className='Profile__form'>
+        <div className='Profile__user-data'>
+          <p className='Profile__text'>Имя</p>
+          <input className='Profile__input' readOnly="readonly"></input>
+        </div>
+        <div className='Profile__user-data'>
+          <p className='Profile__text'>Почта</p>
+          <input className='Profile__input' readOnly="readonly"></input>
+        </div>
+        <button className='Profile__button-form' type='button' onClick={switchEdit}>Сохранить</button>
+      </form>
+      <div className='Profile__buttons'>
+        <button className='Profile__button-edit' onClick={switchEdit}>Редактировать</button>
+        <button className='Profile__button-output'>Выйти из аккаунта</button>
+      </div>
     </div>
   );
 }
