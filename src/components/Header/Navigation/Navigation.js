c221a12cb4cd4385d419aff110148b3c50782714
KVADRAT <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-08-21 18:02:15 +0300

    level-2/21.08.2021/1

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -3,64 +3,67 @@ import { Link, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import icon from '../../../images/Header/Navigation/Navigation__icon-nav.svg';
 
+
+
 function Navigation() {
-  
-	const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    const NavigationLinkAll = document.querySelectorAll('.Navigation__link')
-
-    NavigationLinkAll.forEach((item) => {
-      if(item.classList.contains('activePage')) {
-        item.classList.remove('activePage');
-      }
-    })
-
-		if (pathname === '/movies') {
-      NavigationLinkAll[0].classList.add('activePage')
-    } else if (pathname === '/saved-movies') {
-      NavigationLinkAll[1].classList.add('activePage')
-    } else if (pathname === '/profile') {
-      NavigationLinkAll[2].classList.add('activePage')
-    } else { return }
-	}, [pathname]);
-
-  window.addEventListener(`resize`, () => {
     const NavigationList = document.querySelector('.Navigation__list');
+    const NavigationBackground = document.querySelector('.Navigation__background');
     let screenWidth = document.documentElement.clientWidth;
-    
-    if (screenWidth > 1279 && !NavigationList.classList.contains('hide')) {
+    if (screenWidth < 1280) {
       NavigationList.classList.add('hide');
+      NavigationBackground.classList.add('hide');
+    } else { return };
+  })
+
+  window.addEventListener('resize', () => {
+    const NavigationList = document.querySelector('.Navigation__list');
+    const NavigationBackground = document.querySelector('.Navigation__background');
+    let screenWidth = document.documentElement.clientWidth;
+
+    if (screenWidth < 1280) {
+      NavigationList.classList.add('hide');
+      NavigationBackground.classList.add('hide');
+    } else {
+      NavigationList.classList.remove('hide');
+      NavigationBackground.classList.add('hide');
     }
-  }, false);
+  });
 
   function openMenu(e) {
     const NavigationList = document.querySelector('.Navigation__list');
+    const NavigationBackground = document.querySelector('.Navigation__background');
     let screenWidth = document.documentElement.clientWidth;
 
     if (screenWidth > 1279) { return };
-    if (e.target.className !== 'Navigation') { return };
-    if (!NavigationList.classList.contains('hide')) { return }
+    if (e.target.className !== 'Navigation__button-open-nav') { return };
+    NavigationBackground.classList.remove('hide');
     NavigationList.classList.remove('hide');
   }
 
   function closeMenu() {
     const NavigationList = document.querySelector('.Navigation__list');
+    const NavigationBackground = document.querySelector('.Navigation__background');
     NavigationList.classList.add('hide');
+    NavigationBackground.classList.add('hide');
   }
 
   return (
-    <div className="Navigation" onClick={openMenu}>
-      <ul className="Navigation__list hide">
-        <button className='Navigation__button-Close' onClick={closeMenu}></button>
+    <div className="Navigation">
+      <button className='Navigation__button-open-nav' onClick={openMenu}></button>
+      <div className='Navigation__background hide' onClick={closeMenu}></div>
+      <ul className="Navigation__list">
+      <button className='Navigation__button-Close' onClick={closeMenu}></button>
         <li className="Navigation__item">
-          <Link className='Navigation__link' to='/movies'>Фильмы</Link>
+          <Link className={`Navigation__link ${pathname === '/movies' && 'activePage'}`} to='/movies'>Фильмы</Link>
         </li>
         <li className="Navigation__item">
-          <Link className='Navigation__link' to='/saved-movies'>Сохранённые фильмы</Link>
+          <Link className={`Navigation__link ${pathname === '/saved-movies' && 'activePage'}`} to='/saved-movies'>Сохранённые фильмы</Link>
         </li>
         <li className="Navigation__item">
-          <Link className='Navigation__link' to='/profile'>
+          <Link className={`Navigation__link ${pathname === '/profile' && 'activePage'}`} to='/profile'>
             <p className='Navigation__text-profile'>Аккаунт</p>
             <img className='Navigation__icon-profile' src={icon} alt={'Иконка'}/>
           </Link>
--- src/components/reusableComponents/Authorization/Authorization.js
+++ src/components/reusableComponents/Authorization/Authorization.js
@@ -13,16 +13,25 @@ function Authorization() {
             {pathname === '/sign-up' && 
                 <>
                     <p className='Authorization__text-input'>Имя</p>
-                    <input className='Authorization__input'></input>
+                    <div className='Authorization__section-input'>
+                        <input className='Authorization__input' type='text' required></input>
+                        <span className='Authorization__input-error hide'>Что-то пошло не так...</span>
+                    </div>
                 </>
             }
             <p className='Authorization__text-input'>E-mail</p>
-            <input className='Authorization__input'></input>
+            <div className='Authorization__section-input'>
+                <input className='Authorization__input' type='mail' required></input>
+                <span className='Authorization__input-error hide'>Что-то пошло не так...</span>
+            </div>
             <p className='Authorization__text-input'>Пароль</p>
-            <input className='Authorization__input'></input>
+            <div className='Authorization__section-input'>
+                <input className='Authorization__input' type='password' required></input>
+                <span className='Authorization__input-error hide'>Что-то пошло не так...</span>
+            </div>
             <button className='Authorization__button'>{pathname === '/sign-in' ? 'Войти' : 'Зарегистрироваться'}</button>
         </form>
-        <p className='Authorization__text-link'>{pathname === '/sign-in' ? 'Ещё не зарегистрированы?' : 'Уже зарегистрированы?'} <Link className='Authorization__link' to={pathname === '/sign-in' ? '/sign-up' : '/sign-in'}>{pathname === '/sign-in' ? 'Регистрация' : 'Войти'}</Link></p>
+        <p className='Authorization__text-link'>{pathname === '/sign-in' ? 'Ещё не зарегистрированы?' : 'Уже зарегистрированы?'}<Link className='Authorization__link' to={pathname === '/sign-in' ? '/sign-up' : '/sign-in'}>{pathname === '/sign-in' ? 'Регистрация' : 'Войти'}</Link></p>
     </div>
   );
 }
